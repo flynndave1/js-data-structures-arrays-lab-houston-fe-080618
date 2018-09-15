f3c996cefb05e289a3eddb0c484436d9a2ff6a96
david m flynn <flynndave1@msn.com>
2018-09-15 00:54:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function  removeLastDriver() {
 removeLastDriver();
 
 function  removeFirstDriver() {
- var againFewDrivers = drivers.slice();
+ var againFewDrivers = drivers.slice(1 , drivers.lenght());
   return againFewDrivers ;
 }
 removeFirstDriver();
